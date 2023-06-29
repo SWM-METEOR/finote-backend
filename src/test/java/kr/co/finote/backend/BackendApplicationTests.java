305c4d6c3a306433c390f5649114eb8cd79fdaec
Yerim Hyun <108649711+EllieHyun@users.noreply.github.com>
2023-06-29 14:51:22 +0900

    Initial commit

---
+++ src/test/java/kr/co/finote/backend/BackendApplicationTests.java
@@ -0,0 +1,14 @@
+package kr.co.finote.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
