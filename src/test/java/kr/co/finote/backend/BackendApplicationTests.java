759f1bb041b421f18b65ae792a0b437ebaf9bd91
Yerim Hyun <108649711+EllieHyun@users.noreply.github.com>
2023-06-29 15:03:39 +0900

    Spotless setting
    - 자동 코드 포맷팅을 위한 spotless 설정
    - spotless 적용

--- src/test/java/kr/co/finote/backend/BackendApplicationTests.java
+++ src/test/java/kr/co/finote/backend/BackendApplicationTests.java
@@ -2,13 +2,10 @@ package kr.co.finote.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 class BackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {}
 }
