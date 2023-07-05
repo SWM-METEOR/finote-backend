bda84a0c50b1cc99070316022d85ea43de6a0947
swjeong98 <tree5678@konkuk.ac.kr>
2023-07-05 17:16:12 +0900

    feat: 구글 OAuth 로그인 구현

---
+++ src/main/java/kr/co/finote/backend/src/user/domain/User.java
@@ -0,0 +1,46 @@
+package kr.co.finote.backend.src.user.domain;
+
+import kr.co.finote.backend.global.entity.BaseEntity;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.sql.Timestamp;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String username;
+    private String password;
+    private String email;
+    private String role;
+
+    private String provider;
+    private String providerId;
+
+    @CreationTimestamp
+    private Timestamp createDate;
+
+    @Builder
+    public User(String username, String password, String email, String role, String provider,
+                String providerId, Timestamp createDate) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.role = role;
+        this.provider = provider;
+        this.providerId = providerId;
+        this.createDate = createDate;
+    }
+}
---
+++ src/main/java/kr/co/finote/backend/src/user/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package kr.co.finote.backend.src.user.repository;
+
+import kr.co.finote.backend.src.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    public User findByUsername(String username);
+}
