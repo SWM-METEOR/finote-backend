8ce69ec0c81a6ee6a8bcd34ed36db0aff6d11449
Yerim Hyun <108649711+EllieHyun@users.noreply.github.com>
2023-07-13 17:09:47 +0900

    [FIN-77] feat: 서버 스웨거 연동
    - 스웨거 연동 설정
    - 로그인 api 이름 api 명세서에 맞게 수정
    - gitignore에 DS_Store 추가

--- src/main/java/kr/co/finote/backend/src/user/api/LoginApi.java
+++ src/main/java/kr/co/finote/backend/src/user/api/LoginApi.java
@@ -10,11 +10,13 @@ import kr.co.finote.backend.src.user.service.LoginService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/users")
 @Slf4j
 public class LoginApi {
 
@@ -33,7 +35,7 @@ public class LoginApi {
         }
     }
 
-    @GetMapping("/login/oauth2/code/google/")
+    @GetMapping("/auth/google")
     public GoogleLoginResponseDto auth(@RequestParam String code) {
         log.info("Code from Google social Login API {}", code);
 
