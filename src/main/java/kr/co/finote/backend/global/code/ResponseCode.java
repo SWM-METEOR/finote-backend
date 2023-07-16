82572714c5c95395647e06bed466037271e46996
Sangwon Jeong <86451540+swJeong98@users.noreply.github.com>
2023-07-16 15:24:02 +0900

    [FIN-91] 유저의 블로그 정보, 닉네임 조회 API (#19)
    * [FIN-91] 유저의 블로그 정보, 닉네임 조회 API
    
    - /blogs/blog-info, /users/nickname API 구현
    - 세션이 없는 경우 예외 발생하도록
    - 세션이 있는 경우 해당 유저의 정보 반환

--- src/main/java/kr/co/finote/backend/global/code/ResponseCode.java
+++ src/main/java/kr/co/finote/backend/global/code/ResponseCode.java
@@ -17,8 +17,14 @@ public enum ResponseCode {
 
     INVALID_INPUT_VALUE(BAD_REQUEST, "400_BAD_REQUEST", "입력값이 올바르지 않습니다."),
 
+    /** UsersBlog error */
+    BLOG_NOT_FOUND(NOT_FOUND, "404_BLOG_NOT_FOUND", "유저의 블로그를 찾을 수 없습니다."),
+
     /** article error */
-    ARTICLE_NOT_FOUND(NOT_FOUND, "404_ARTICLE_NOT_FOUND", "해당 게시글을 찾을 수 없습니다.");
+    ARTICLE_NOT_FOUND(NOT_FOUND, "404_ARTICLE_NOT_FOUND", "해당 게시글을 찾을 수 없습니다."),
+
+    /** error occurs when unauthenticated user requests for resource */
+    UNAUTHENTICATED(UNAUTHORIZED, "401_UNAUTHORIZED", "인증되지 않은 사용자입니다.");
 
     private final HttpStatus status;
     private final String code;
--- src/main/java/kr/co/finote/backend/src/blog/service/UsersBlogService.java
+++ src/main/java/kr/co/finote/backend/src/blog/service/UsersBlogService.java
@@ -1,6 +1,10 @@
 package kr.co.finote.backend.src.blog.service;
 
+import kr.co.finote.backend.global.code.ResponseCode;
+import kr.co.finote.backend.global.exception.CustomException;
+import kr.co.finote.backend.src.blog.domain.UsersBlog;
 import kr.co.finote.backend.src.blog.repository.UsersBlogRepository;
+import kr.co.finote.backend.src.user.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -16,5 +20,11 @@ public class UsersBlogService {
 
     public boolean duplicateBlogUrl(String url) {
         return usersBlogRepository.existsByUrl(url);
+    }
+
+    public UsersBlog getUsersBlog(User user) {
+        return usersBlogRepository
+                .findByUserAndIsDeleted(user, false)
+                .orElseThrow(() -> new CustomException(ResponseCode.BLOG_NOT_FOUND));
     }
 }
