d6823cd1d5deb2d8037c88eb7eea80c5b5f749eb
swjeong98 <tree5678@konkuk.ac.kr>
2023-07-25 23:15:19 +0900

    [FIN-119]  AI 검색(코드 설명) API 구현
    - \n으로 처리되는 코드에 대해서만 우선 테스트 완료
    - 팀원들과 이 경우에 대해 이야기해봐야 합니다!

--- src/main/java/kr/co/finote/backend/src/article/api/AiSearchApi.java
+++ src/main/java/kr/co/finote/backend/src/article/api/AiSearchApi.java
@@ -1,5 +1,6 @@
 package kr.co.finote.backend.src.article.api;
 
+import io.swagger.v3.oas.annotations.Operation;
 import javax.validation.Valid;
 import kr.co.finote.backend.src.article.domain.AiSearchType;
 import kr.co.finote.backend.src.article.dto.request.AiSearchRequest;
@@ -21,6 +22,7 @@ public class AiSearchApi {
 
     private final AiSearchService aiSearchService;
 
+    @Operation(summary = "배우기 - 의미")
     @GetMapping("/ai-search/meaning")
     public AiSearchResponse meaning(@RequestBody @Valid AiSearchRequest aiSearchRequest) {
 
@@ -28,10 +30,19 @@ public class AiSearchApi {
         return aiSearchService.getResponse(prompt, AiSearchType.MEANING);
     }
 
+    @Operation(summary = "배우기 - 추가 설명")
     @GetMapping("/ai-search/explanation")
     public AiSearchResponse explanation(@RequestBody @Valid AiSearchRequest aiSearchRequest) {
 
         String prompt = aiSearchRequest.getPrompt() + ChatGptPromptUtils.EXPLAIN_SUFFIX;
         return aiSearchService.getResponse(prompt, AiSearchType.EXPLANATION);
+    }
+
+    @Operation(summary = "배우기 - 코드 설명")
+    @GetMapping("/ai-search/code")
+    public AiSearchResponse code(@RequestBody @Valid AiSearchRequest aiSearchRequest) {
+
+        String prompt = aiSearchRequest.getPrompt() + ChatGptPromptUtils.CODE_SUFFIX;
+        return aiSearchService.getResponse(prompt, AiSearchType.CODE);
     }
 }
