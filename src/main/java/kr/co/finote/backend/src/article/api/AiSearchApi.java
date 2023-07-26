ca15007be775c39ec878d8745f8cfb2fca621d57
Sangwon Jeong <86451540+swJeong98@users.noreply.github.com>
2023-07-26 11:28:52 +0900

    [FIN-119] AI 검색 (코드 설명) API (#29)
    * [FIN-119]  AI 검색(코드 설명) API 구현
    
    - \n으로 처리되는 코드에 대해서만 우선 테스트 완료
    - 팀원들과 이 경우에 대해 이야기해봐야 합니다!
    - @Operation 에 해당 내용 반영 (배우기 -> AI 검색)

--- src/main/java/kr/co/finote/backend/src/article/api/AiSearchApi.java
+++ src/main/java/kr/co/finote/backend/src/article/api/AiSearchApi.java
@@ -1,5 +1,6 @@
 package kr.co.finote.backend.src.article.api;
 
+import io.swagger.v3.oas.annotations.Operation;
 import javax.validation.Valid;
 import kr.co.finote.backend.src.article.domain.AiSearchType;
 import kr.co.finote.backend.src.article.dto.request.AiSearchRequest;
@@ -8,10 +9,7 @@ import kr.co.finote.backend.src.article.service.AiSearchService;
 import kr.co.finote.backend.src.article.utils.ChatGptPromptUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -21,17 +19,27 @@ public class AiSearchApi {
 
     private final AiSearchService aiSearchService;
 
-    @GetMapping("/ai-search/meaning")
+    @Operation(summary = "AI 검색 - 의미")
+    @PostMapping("/ai-search/meaning")
     public AiSearchResponse meaning(@RequestBody @Valid AiSearchRequest aiSearchRequest) {
 
         String prompt = aiSearchRequest.getPrompt() + ChatGptPromptUtils.MEANING_SUFFIX;
         return aiSearchService.getResponse(prompt, AiSearchType.MEANING);
     }
 
-    @GetMapping("/ai-search/explanation")
+    @Operation(summary = "AI 검색 - 추가 설명")
+    @PostMapping("/ai-search/explanation")
     public AiSearchResponse explanation(@RequestBody @Valid AiSearchRequest aiSearchRequest) {
 
         String prompt = aiSearchRequest.getPrompt() + ChatGptPromptUtils.EXPLAIN_SUFFIX;
         return aiSearchService.getResponse(prompt, AiSearchType.EXPLANATION);
+    }
+
+    @Operation(summary = "AI 검색 - 코드 설명")
+    @PostMapping("/ai-search/code")
+    public AiSearchResponse code(@RequestBody @Valid AiSearchRequest aiSearchRequest) {
+
+        String prompt = aiSearchRequest.getPrompt() + ChatGptPromptUtils.CODE_SUFFIX;
+        return aiSearchService.getResponse(prompt, AiSearchType.CODE);
     }
 }
--- src/main/java/kr/co/finote/backend/src/article/utils/ChatGptPromptUtils.java
+++ src/main/java/kr/co/finote/backend/src/article/utils/ChatGptPromptUtils.java
@@ -4,4 +4,5 @@ public class ChatGptPromptUtils {
 
     public static final String MEANING_SUFFIX = "에 대해 의미는 5줄 이내 글로만 설명해줘";
     public static final String EXPLAIN_SUFFIX = "에 대해 추가 설명을 10줄 이내 글로만 설명해줘";
+    public static final String CODE_SUFFIX = "이 코드를 10줄 이내로 설명해줘";
 }
