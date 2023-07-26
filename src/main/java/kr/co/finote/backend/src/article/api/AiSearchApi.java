cef5a86e6bbccd39cfaaca617fc82f5ae8bef65f
swjeong98 <tree5678@konkuk.ac.kr>
2023-07-26 09:38:43 +0900

    [FIN-119]  AI 검색(코드 설명) API 구현
    - RequestBody 가 있는 API의 Method를 POST로 수정
    - @Operation 수정 (배우기 -> AI 검색)

--- src/main/java/kr/co/finote/backend/src/article/api/AiSearchApi.java
+++ src/main/java/kr/co/finote/backend/src/article/api/AiSearchApi.java
@@ -9,10 +9,7 @@ import kr.co.finote.backend.src.article.service.AiSearchService;
 import kr.co.finote.backend.src.article.utils.ChatGptPromptUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -22,24 +19,24 @@ public class AiSearchApi {
 
     private final AiSearchService aiSearchService;
 
-    @Operation(summary = "배우기 - 의미")
-    @GetMapping("/ai-search/meaning")
+    @Operation(summary = "AI 검색 - 의미")
+    @PostMapping("/ai-search/meaning")
     public AiSearchResponse meaning(@RequestBody @Valid AiSearchRequest aiSearchRequest) {
 
         String prompt = aiSearchRequest.getPrompt() + ChatGptPromptUtils.MEANING_SUFFIX;
         return aiSearchService.getResponse(prompt, AiSearchType.MEANING);
     }
 
-    @Operation(summary = "배우기 - 추가 설명")
-    @GetMapping("/ai-search/explanation")
+    @Operation(summary = "AI 검색 - 추가 설명")
+    @PostMapping("/ai-search/explanation")
     public AiSearchResponse explanation(@RequestBody @Valid AiSearchRequest aiSearchRequest) {
 
         String prompt = aiSearchRequest.getPrompt() + ChatGptPromptUtils.EXPLAIN_SUFFIX;
         return aiSearchService.getResponse(prompt, AiSearchType.EXPLANATION);
     }
 
-    @Operation(summary = "배우기 - 코드 설명")
-    @GetMapping("/ai-search/code")
+    @Operation(summary = "AI 검색 - 코드 설명")
+    @PostMapping("/ai-search/code")
     public AiSearchResponse code(@RequestBody @Valid AiSearchRequest aiSearchRequest) {
 
         String prompt = aiSearchRequest.getPrompt() + ChatGptPromptUtils.CODE_SUFFIX;
