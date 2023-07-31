8c99a6f0aad772b76f6400180e73c12efa46fe42
soma-devops <hyunyelim25@gmail.com>
2023-07-31 21:54:25 +0900

    feat: CD 구축
    - appspec overwrite 옵션 추가
    - deploy script 파일 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,16 +1,20 @@
-version: 0.0
-os: linux
+REPOSITORY=/home/ubuntu/finote/finote-backend
+cd $REPOSITORY
 
-files:
-  - source: /
-    destination: /home/ubuntu/finote/finote-backend
-    overwrite: yes
-permissions:
-  - object: /home/ubuntu/finote/finote-backend/
-    owner: ubuntu
-    group: ubuntu
-hooks:
-  AfterInstall:
-    - location: scripts/deploy.sh
-      timeout: 60
-      runas: ubuntu
+APP_NAME=finote
+JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
+JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
+
+CURRENT_PID=$(pgrep -f $APP_NAME)
+
+if [ -z $CURRENT_PID ]
+then
+  echo "> 종료할 애플리케이션이 없습니다."
+else
+  echo "> kill -9 $CURRENT_PID"
+  kill -15 $CURRENT_PID
+  sleep 5
+fi
+
+echo "> Deploy - $JAR_PATH "
+nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
