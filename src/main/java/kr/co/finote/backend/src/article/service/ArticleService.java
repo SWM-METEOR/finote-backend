33b31283825214fb443c8fcff05dcd67e2500d60
soma-devops <hyunyelim25@gmail.com>
2023-08-07 15:40:28 +0900

    [FIN-167] feat: 서버 에러 로그 slack 연동
    - 서버 에러 로그를 slack에서 모두 확인할 수 있도록 logback와 slack 연동

--- src/main/java/kr/co/finote/backend/src/article/service/ArticleService.java
+++ src/main/java/kr/co/finote/backend/src/article/service/ArticleService.java
@@ -6,6 +6,7 @@ import kr.co.finote.backend.src.article.domain.Article;
 import kr.co.finote.backend.src.article.dto.request.ArticleRequest;
 import kr.co.finote.backend.src.article.repository.ArticleRepository;
 import kr.co.finote.backend.src.user.domain.User;
+import kr.co.finote.backend.src.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -14,9 +15,14 @@ import org.springframework.stereotype.Service;
 public class ArticleService {
 
     private final ArticleRepository articleRepository;
+    private final UserRepository userRepository;
 
     public Long save(ArticleRequest articleRequest, User loginUser) {
-        Article article = Article.createArticle(articleRequest, loginUser);
+        User user =
+                userRepository
+                        .findByEmailAndIsDeleted("hyunyelim25@gmail.com", false)
+                        .orElseThrow(() -> new NotFoundException(ResponseCode.USER_NOT_FOUND));
+        Article article = Article.createArticle(articleRequest, user); // TODO 로그인 후 login user로 변경
         return articleRepository.save(article).getId();
     }
 
