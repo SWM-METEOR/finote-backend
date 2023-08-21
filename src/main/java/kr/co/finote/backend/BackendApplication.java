a0d02747221f4e743adcfb9971f5c3ac4e1f3c3c
Sangwon Jeong <tree5678@konkuk.ac.kr>
2023-08-21 14:46:02 +0900

    [FIN-223] 배포 서버 CI/CD 구축 - Context Failed 오류 해결 위해 수정

--- src/main/java/kr/co/finote/backend/BackendApplication.java
+++ src/main/java/kr/co/finote/backend/BackendApplication.java
@@ -1,13 +1,15 @@
 package kr.co.finote.backend;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class BackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
-        System.out.println("CI/CD Test !");
+        log.info("CI/CD Test");
     }
 }
