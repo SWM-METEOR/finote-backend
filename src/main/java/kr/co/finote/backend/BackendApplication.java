597cee19747989307d0a8b8e6ba3189074862cb6
Sangwon Jeong <tree5678@konkuk.ac.kr>
2023-08-21 18:20:23 +0900

    [FIN-223] 배포 서버 CI/CD 구축 - 배포 확인용 커밋

--- src/main/java/kr/co/finote/backend/BackendApplication.java
+++ src/main/java/kr/co/finote/backend/BackendApplication.java
@@ -4,12 +4,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.LocalDateTime;
+
 @SpringBootApplication
 @Slf4j
 public class BackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
-        log.info("CI/CD Test");
+        log.info("Server Start Time : {}", LocalDateTime.now());
     }
 }
