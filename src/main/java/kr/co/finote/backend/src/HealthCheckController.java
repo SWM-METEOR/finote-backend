2e60aaa542503d6a8fc499cadd74fed4fc210749
Sangwon Jeong <tree5678@konkuk.ac.kr>
2023-08-21 15:08:22 +0900

    [FIN-223] 배포 서버 CI/CD 구축 - Health Check를 위한 Controller 추가

---
+++ src/main/java/kr/co/finote/backend/src/HealthCheckController.java
@@ -0,0 +1,12 @@
+package kr.co.finote.backend.src;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HealthCheckController {
+    @GetMapping("/health-check")
+    public void getHealthCheck() {}
+}
