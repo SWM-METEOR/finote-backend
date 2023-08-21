02c9029fc91ab772f6668009c081c583d1c6190f
Sangwon Jeong <tree5678@konkuk.ac.kr>
2023-08-21 13:59:42 +0900

    [FIN-223] 배포 서버 CI/CD 구축 - ci/cd를 위한 쉘, yml 작성

---
+++ scripts/deploy.sh
@@ -0,0 +1,25 @@
+REPOSITORY=/home/ubuntu/finote/finote-backend
+# shellcheck disable=SC2164
+cd $REPOSITORY
+
+# shellcheck disable=SC2010
+JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
+JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
+
+CURRENT_PID=$(lsof -t -i :8080)
+
+if [ -z "$CURRENT_PID" ]; then
+  echo "> 종료할 애플리케이션이 없습니다."
+else
+  echo "> kill -9 $CURRENT_PID"
+  kill -15 "$CURRENT_PID"
+  sleep 5
+fi
+
+echo "> Deploy - $JAR_PATH "
+./gradlew clean build
+
+BUILD_REPOSITORY=/home/ubuntu/finote/finote-backend/build/libs
+# shellcheck disable=SC2164
+cd $BUILD_REPOSITORY
+nohup java -jar "$JAR_NAME" > /dev/null 2> /dev/null < /dev/null &
