eb5a39c2a981c35aa9d759140a59cadeb28dd790
Sangwon Jeong <tree5678@konkuk.ac.kr>
2023-09-05 23:42:53 +0900

    [FIN-304] S3 업로드를 위한 Pre-signed url 발급 API - 리뷰어 피드백 반영

--- src/main/java/kr/co/finote/backend/src/common/dto/request/FileUploadRequest.java
+++ src/main/java/kr/co/finote/backend/src/common/dto/request/FileUploadRequest.java
@@ -1,14 +1,12 @@
 package kr.co.finote.backend.src.common.dto.request;
 
 import lombok.*;
-import lombok.experimental.FieldDefaults;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-@FieldDefaults(level = AccessLevel.PRIVATE)
 public class FileUploadRequest {
 
-    String fileName;
+    private String fileName;
 }
--- src/main/java/kr/co/finote/backend/src/common/dto/response/PresignedUrlResponse.java
+++ src/main/java/kr/co/finote/backend/src/common/dto/response/PresignedUrlResponse.java
@@ -1,18 +1,15 @@
 package kr.co.finote.backend.src.common.dto.response;
 
-import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.experimental.FieldDefaults;
 
 @Getter
 @AllArgsConstructor
 @Builder
-@FieldDefaults(level = AccessLevel.PRIVATE)
 public class PresignedUrlResponse {
 
-    String preSignedUrl;
+    private String preSignedUrl;
 
     public static PresignedUrlResponse createdPresignedUrlResponse(String preSignedUrl) {
         return PresignedUrlResponse.builder().preSignedUrl(preSignedUrl).build();
