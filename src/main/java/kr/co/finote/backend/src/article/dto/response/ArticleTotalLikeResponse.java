5e025a355b3462f2649a3a919c78fd762d904bf7
Sangwon Jeong <86451540+swJeong98@users.noreply.github.com>
2023-09-11 09:54:30 +0900

    [FIN-316] feat: 글의 총 좋아요 수 API (#114)
    * [FIN-316] 글 좋아요 수 API
    
    - 글의 총 좋아요 수를 반환하는 API 추가

---
+++ src/main/java/kr/co/finote/backend/src/article/dto/response/ArticleTotalLikeResponse.java
@@ -0,0 +1,15 @@
+package kr.co.finote.backend.src.article.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class ArticleTotalLikeResponse {
+
+    private int totalLike;
+
+    public static ArticleTotalLikeResponse createArticleTotalLikeResponse(int totalLike) {
+        return ArticleTotalLikeResponse.builder().totalLike(totalLike).build();
+    }
+}
