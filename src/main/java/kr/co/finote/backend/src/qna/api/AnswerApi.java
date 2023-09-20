4eb3ea4798c44c328219db84475b1f075e823777
Sangwon Jeong <tree5678@konkuk.ac.kr>
2023-09-20 23:19:46 +0900

    [FIN-360] 질문글 답변 리스트 API
    - 질문글 답변 리스트 API 작성
    - fetch join 활용

--- src/main/java/kr/co/finote/backend/src/qna/api/AnswerApi.java
+++ src/main/java/kr/co/finote/backend/src/qna/api/AnswerApi.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import javax.validation.Valid;
 import kr.co.finote.backend.global.annotation.Login;
 import kr.co.finote.backend.src.qna.dto.request.PostAnswerRequest;
+import kr.co.finote.backend.src.qna.dto.response.AnswerListResponse;
 import kr.co.finote.backend.src.qna.dto.response.PostAnswerResponse;
 import kr.co.finote.backend.src.qna.service.AnswerService;
 import kr.co.finote.backend.src.user.domain.User;
@@ -30,5 +31,11 @@ public class AnswerApi {
             @PathVariable String title,
             @RequestBody @Valid PostAnswerRequest request) {
         return answerService.postAnswer(loginUser, nickname, title, request);
+    }
+
+    @Operation(summary = "질문 글에 대한 모든 답변 조회")
+    @GetMapping("/answers/{nickname}/{title}")
+    public AnswerListResponse getAnswers(@PathVariable String nickname, @PathVariable String title) {
+        return answerService.getAnswers(nickname, title);
     }
 }
--- src/main/java/kr/co/finote/backend/src/qna/service/AnswerService.java
+++ src/main/java/kr/co/finote/backend/src/qna/service/AnswerService.java
@@ -1,8 +1,12 @@
 package kr.co.finote.backend.src.qna.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import kr.co.finote.backend.src.qna.domain.Answer;
 import kr.co.finote.backend.src.qna.domain.Question;
 import kr.co.finote.backend.src.qna.dto.request.PostAnswerRequest;
+import kr.co.finote.backend.src.qna.dto.response.AnswerListResponse;
+import kr.co.finote.backend.src.qna.dto.response.AnswerResponse;
 import kr.co.finote.backend.src.qna.dto.response.PostAnswerResponse;
 import kr.co.finote.backend.src.qna.repository.AnswerRepository;
 import kr.co.finote.backend.src.user.domain.User;
@@ -23,5 +27,18 @@ public class AnswerService {
 
         Answer savedAnswer = answerRepository.save(answer);
         return PostAnswerResponse.of(savedAnswer);
+    }
+
+    public AnswerListResponse getAnswers(String nickname, String title) {
+        Question question = questionService.findByNicknameAndTitle(nickname, title);
+
+        List<Answer> answers = answerRepository.findAllWithQuestion(question);
+        List<AnswerResponse> answerReponseList = toAnswerReponseList(answers);
+
+        return AnswerListResponse.of(answerReponseList);
+    }
+
+    private List<AnswerResponse> toAnswerReponseList(List<Answer> answerList) {
+        return answerList.stream().map(AnswerResponse::of).collect(Collectors.toList());
     }
 }
