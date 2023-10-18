df5eb9b3d55f9326b704279270e3c531d3a58bc1
Sangwon Jeong <tree5678@konkuk.ac.kr>
2023-10-18 13:57:17 +0900

    [FIN-422] 메시지 큐 통신 환경 구축 - pmdTest 적용

--- src/test/java/kr/co/finote/backend/src/common/utils/SqsSenderTest.java
+++ src/test/java/kr/co/finote/backend/src/common/utils/SqsSenderTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import kr.co.finote.backend.src.article.dto.request.FeedRequest;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,6 +19,6 @@ class SqsSenderTest {
     @Test
     void sendMessage() throws JsonProcessingException {
         FeedRequest request = FeedRequest.createFeedRequest("abcd", 3L);
-        sqsSender.sendMessage(request);
+        Assertions.assertThat(sqsSender.sendMessage(request)).isNotNull();
     }
 }
